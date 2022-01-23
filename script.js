2b24c1942a91a3f4ecbf29090e26ec5c8a944b2b
Mohammad Joud <judejulius12@gmail.com>
2022-01-23 21:08:06 +0200

    first commit

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
         var w=document.getElementById('w').value;
         var bmi=w/(h/100*h/100);
         var bmio=(bmi.toFixed(2));
-
         document.getElementById("result").innerHTML="Your BMI is " + bmio;
+
+        if(bmio <= 25 && bmio > 40){
+            document.getElementById("result1").innerHTML = "You are Overweight"
+        }else if(bmio >= 18 && bmio <= 25){
+            document.getElementById("result1").innerHTML = "You have a good BMI"
+        }else{
+            document.getElementById("result1").innerHTML = "You are UnderWeight"
+        }
+       
     }
